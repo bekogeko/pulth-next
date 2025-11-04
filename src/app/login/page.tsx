e12532745885c7008d87079f19cb7ebb384bda72
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-04 16:00:52 +0100

    Adds user roles and updates login/register paths.
    Introduces a 'role' field to the User model with a default value of "user".
    Refactors import paths for LoginForm and RegisterForm to reflect their
    new locations.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,11 @@
-import Link from "next/link";
-import { LoginForm } from "@/app/ui/loginForm";
+import {LoginForm} from "@/app/login/loginForm";
 
 export default function Login() {
     return (
-            <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-                <div className="w-full max-w-sm">
-                    <LoginForm />
-                </div>
+        <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm">
+                <LoginForm/>
             </div>
+        </div>
     );
 }
