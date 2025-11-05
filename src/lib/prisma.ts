d30ab6c1b223f8ac7e1cd952364785f9513d8dfe
Bekir Gülestan <gulestanbekir@gmail.com>
2025-11-05 15:31:41 +0100

    Configures Prisma for optimized database connection
    Configures Prisma to use a direct database connection
    for improved performance in non-production environments.
    
    This change ensures that Prisma utilizes connection pooling in production
    while bypassing it in development and testing for faster iteration.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from '@/generated/prisma-client'
+import {PrismaClient} from '@/generated/prisma-client'
+import {env} from 'process';
 
 const globalForPrisma = global as unknown as {
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient({
+    datasources: {
+        db: {
+            url: env.DATABASE_URL,
+        }
+    }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
